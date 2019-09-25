b32873a42ed9da1005af276c3ed8d2d33f184693
Pete Andrew <peteandrew101@gmail.com>
2019-09-25 22:00:23 +0100

    Added fade example

---
+++ fade/fade.c
@@ -0,0 +1,127 @@
+//
+// Based on code from https://www.embeddedrelated.com/showarticle/528.php
+// 8MHz internal osc
+//
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdint.h>
+#include <string.h>
+#include <avr/pgmspace.h>
+
+#define NUM_WS2812        1
+#define NUM_LEDS          (NUM_WS2812*3)
+
+typedef struct RgbColor
+{
+    unsigned char r;
+    unsigned char g;
+    unsigned char b;
+} RgbColor;
+
+typedef struct HsvColor
+{
+    unsigned char h;
+    unsigned char s;
+    unsigned char v;
+} HsvColor;
+
+// declaration of our ASM function
+extern void output_grb(uint8_t * ptr, uint16_t count);
+
+void set_color(uint8_t * p_buf, uint8_t led, uint8_t r, uint8_t g, uint8_t b)
+{
+  uint16_t index = 3*led;
+  p_buf[index++] = g;
+  p_buf[index++] = r;
+  p_buf[index] = b;  
+}
+
+// Code from https://stackoverflow.com/a/14733008
+RgbColor HsvToRgb(HsvColor hsv)
+{
+  RgbColor rgb;
+  unsigned char region, remainder, p, q, t;
+
+  if (hsv.s == 0)
+  {
+    rgb.r = hsv.v;
+    rgb.g = hsv.v;
+    rgb.b = hsv.v;
+    return rgb;
+  }
+
+  region = hsv.h / 43;
+  remainder = (hsv.h - (region * 43)) * 6; 
+
+  p = (hsv.v * (255 - hsv.s)) >> 8;
+  q = (hsv.v * (255 - ((hsv.s * remainder) >> 8))) >> 8;
+  t = (hsv.v * (255 - ((hsv.s * (255 - remainder)) >> 8))) >> 8;
+
+  switch (region)
+  {
+    case 0:
+      rgb.r = hsv.v; rgb.g = t; rgb.b = p;
+      break;
+    case 1:
+      rgb.r = q; rgb.g = hsv.v; rgb.b = p;
+      break;
+    case 2:
+      rgb.r = p; rgb.g = hsv.v; rgb.b = t;
+      break;
+    case 3:
+      rgb.r = p; rgb.g = q; rgb.b = hsv.v;
+      break;
+    case 4:
+      rgb.r = t; rgb.g = p; rgb.b = hsv.v;
+      break;
+    default:
+      rgb.r = hsv.v; rgb.g = p; rgb.b = q;
+      break;
+  }
+
+  return rgb;
+}
+
+int main(void)
+{
+  uint8_t buf[NUM_LEDS];
+
+  HsvColor hsv;
+  hsv.h = 230;
+  hsv.s = 255;
+  hsv.v = 100;
+
+  RgbColor rgb;
+
+  uint8_t up = 1;
+
+  DDRB = 1;   // bit 0 is our output
+
+  while(1)
+  {
+    output_grb(buf, sizeof(buf));
+   
+    memset(buf, 0, sizeof(buf));
+
+    rgb = HsvToRgb(hsv);
+
+    set_color(
+      buf,
+      0,
+      rgb.r,
+      rgb.g,
+      rgb.b
+    );
+
+    if (up) {
+      hsv.v++;
+      if (hsv.v == 255) up = 0;
+    } else {
+      hsv.v--;
+      if (hsv.v == 0) up = 1;
+    }
+
+    _delay_ms(10);
+  }
+}
